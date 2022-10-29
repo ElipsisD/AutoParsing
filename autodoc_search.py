e310f3730ba996751f1e8dbd969ddd70a6d6de14
Dmitry Boyko <da-boyko@mail.ru>
2022-10-29 23:24:23 +0700

    Optimize interaction with cookies

--- autodoc_search.py
+++ autodoc_search.py
@@ -1,3 +1,4 @@
+import selenium.common
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,7 +11,8 @@ import json
 options = webdriver.ChromeOptions()
 options.headless = True  # работа браузера в тихом режиме
 options.add_argument('--disable-blink-features-AutomationControlled')  # отключение режима WebDriver
-options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36')
+options.add_argument(
+    f'user-agent=Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36')
 s = Service(r"E:\Python\autoParsing\chromedriver.exe")
 
 
@@ -38,8 +40,8 @@ def autodoc_parse(partnumber: str, partname: str = None, manufacturer: str = Non
             num, manufacturer, partname = required_manufacturer(d_companies)
         else:
             num = [i.text for i in names].index(partname) + 1
-        names[num - 1].click()
-    except Exception:
+        names[num-1].click()
+    except selenium.common.NoSuchElementException:
         browser.implicitly_wait(8)
         q = browser.find_element(By.TAG_NAME, 'h1')
         manufacturer, _, _, *partname = q.text.split()
@@ -57,36 +59,38 @@ def autodoc_parse(partnumber: str, partname: str = None, manufacturer: str = Non
 def autodoc_auth(browser) -> None:
     """Авторизация на сайте"""
 
-    """Вход с вводом логина и пароля"""
-    browser.find_element(by=By.XPATH, value='//*[@id="loginInfo"]/div/a').click()
-    sleep(2)
-    browser.find_element(by=By.XPATH, value='//*[@id="Login"]').send_keys(auth_Auotodoc.login)  # вставляем логин
-    browser.find_element(by=By.XPATH, value='//*[@id="Password"]').send_keys(auth_Auotodoc.passw)  # вставляем пароль
-    browser.find_element(by=By.XPATH, value='//*[@id="submit_logon_page"]').click()
-    sleep(2)
-    with open(f'{auth_Auotodoc.login}_cookies', 'w') as fin:
-        json.dump(browser.get_cookies(), fin)
-    sleep(3)
-
-    """Вход по заранее сохраненным куки-файлам"""
-    # browser.delete_all_cookies()
-    # with open(f'{auth_Auotodoc.login}_cookies') as fout:
-    #     for cookie in json.load(fout):
-    #         browser.add_cookie(cookie)
-    # sleep(1)
-    # browser.refresh()
-    # sleep(1)
+    try:
+        """Вход по заранее сохраненным куки-файлам"""
+        browser.delete_all_cookies()
+        with open(f'{auth_Auotodoc.login}_cookies') as fout:
+            for cookie in json.load(fout):
+                browser.add_cookie(cookie)
+        sleep(1)
+        browser.refresh()
+        sleep(1)
+    except FileNotFoundError:
+        """Вход с вводом логина и пароля"""
+        browser.find_element(by=By.XPATH, value='//*[@id="loginInfo"]/div/a').click()
+        sleep(2)
+        browser.find_element(by=By.XPATH, value='//*[@id="Login"]').send_keys(auth_Auotodoc.login)  # вставляем логин
+        browser.find_element(by=By.XPATH, value='//*[@id="Password"]').send_keys(
+            auth_Auotodoc.passw)  # вставляем пароль
+        browser.find_element(by=By.XPATH, value='//*[@id="submit_logon_page"]').click()
+        sleep(2)
+        with open(f'{auth_Auotodoc.login}_cookies', 'w') as fin:
+            json.dump(browser.get_cookies(), fin)
+        sleep(3)
 
 
 def required_manufacturer(manufacturers_options: dict) -> tuple:
-    '''Принимает варианты запчастей, подходящие под запрошенный partnumber.
-    Возвращает порядковый номер в спике, производителя и partnumber запчасти в соответствии с выбором клиента.
-    '''
+    """Принимает варианты запчастей, подходящие под запрошенный partnumber.
+    Возвращает порядковый номер в списке, производителя и partnumber запчасти в соответствии с выбором клиента.
+    """
     for i, (key, value) in manufacturers_options.items():
         print(f'{i}) {key} \t {value}')
     answer = input('Введите номер нужного варианта: ')
     return int(answer), *manufacturers_options[int(answer)]
 
 
-# print(autodoc_parse('W7008'))
+print(autodoc_parse('W7008'))
 # print(autodoc_parse('2074151'))
